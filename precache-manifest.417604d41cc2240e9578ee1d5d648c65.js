c752a4adb8fb7bb411a0863a9f934fae6d31fd71
bouchra171 <bouchramidoune09@gmail.com>
2023-09-30 18:50:35 +0200

    Updates

---
+++ precache-manifest.417604d41cc2240e9578ee1d5d648c65.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a317a5e5eec9fc4c577",
+    "url": "/Maths-Heros/static/css/main.4a224f88.chunk.css"
+  },
+  {
+    "revision": "8a317a5e5eec9fc4c577",
+    "url": "/Maths-Heros/static/js/main.8a317a5e.chunk.js"
+  },
+  {
+    "revision": "cf042e081f8a87233510",
+    "url": "/Maths-Heros/static/js/1.cf042e08.chunk.js"
+  },
+  {
+    "revision": "b36ff9166e2729224f69",
+    "url": "/Maths-Heros/static/css/2.38856b6c.chunk.css"
+  },
+  {
+    "revision": "b36ff9166e2729224f69",
+    "url": "/Maths-Heros/static/js/2.b36ff916.chunk.js"
+  },
+  {
+    "revision": "e72119e4b55df2407670",
+    "url": "/Maths-Heros/static/js/runtime~main.e72119e4.js"
+  },
+  {
+    "revision": "4315990f43a23c077994e16734bff3ad",
+    "url": "/Maths-Heros/static/media/imag.4315990f.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Maths-Heros/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Maths-Heros/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Maths-Heros/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Maths-Heros/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Maths-Heros/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "151f13740edc334be391d3c8ac17e94e",
+    "url": "/Maths-Heros/index.html"
+  }
+];
